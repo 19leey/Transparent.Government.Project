a0dbf5d6c5574628b83d56ad692693219c054cc6
Young Lee <lee.youngx@gmail.com>
2020-11-11 00:31:49 -0600

    Testing mongodb integration for dumping ingested data

--- build.sbt
+++ build.sbt
@@ -4,6 +4,8 @@ version := "0.1"
 
 scalaVersion := "2.13.3"
 
+ThisBuild / useCoursier := false
+
 val akkaVersion = "2.6.10"
 val akkaHttpVersion = "10.2.1"
 val playVersion = "2.9.1"
@@ -14,4 +16,6 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
   "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion,
   "com.typesafe.play" %% "play-json" % playVersion,
+  "com.lightbend.akka" %% "akka-stream-alpakka-mongodb" % "2.0.2",
+  "org.mongodb" % "mongodb-driver-reactivestreams" % "1.12.0"
 )
--- src/main/scala/tgp/ingestion/module/Mongo.scala
+++ src/main/scala/tgp/ingestion/module/Mongo.scala
@@ -2,37 +2,107 @@ package tgp.ingestion.module
 
 import java.util.UUID
 
-import akka.actor.Actor
-import akka.actor.typed.scaladsl.adapter.ClassicActorContextOps
-import tgp.ingestion.module.CIDWorker._
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.alpakka.mongodb.scaladsl.MongoSink
+import akka.stream.scaladsl.Source
+import com.mongodb.client.model.Filters
+import com.mongodb.reactivestreams.client.MongoClients
+import play.api.libs.json.Json
 
-object CIDWorker {
 
-  case class Done()
-  case class CID(cid: String, state: String)
-  case class getCandidateSummary(cid: String)
-  case class getCandidateContributors(cid: String)
-  case class getCandidateIndustries(cid: String)
+object Mongo extends App {
 
-}
+  private val client = MongoClients.create("mongodb://localhost:27017")
+  private val db = client.getDatabase("test")
 
-class CIDWorker extends Actor {
-  val apiKey = "a823a42f77a789a884db54ef9669e48d"
+  implicit val system = ActorSystem()
+  implicit val mat = ActorMaterializer()
 
-  override def receive: Receive = {
-    case CID(cid, state) =>
-      val url = s"http://www.opensecrets.org/api/?method=memPFDprofile&apikey=$apiKey&cid=$cid&output=json"
-      println(s"CID WORKER CREATED BY: $state")
-      println(s"CID: $cid URL: $url")
+  val paylaod = """{
+                  |  "response": {
+                  |    "legislator": [
+                  |      {
+                  |        "@attributes": {
+                  |          "cid": "N00038414",
+                  |          "firstlast": "Lisa Blunt Rochester",
+                  |          "lastname": "Rochester",
+                  |          "party": "D",
+                  |          "office": "DE01",
+                  |          "gender": "F",
+                  |          "first_elected": "2016",
+                  |          "exit_code": "0",
+                  |          "comments": "",
+                  |          "phone": "202-225-4165",
+                  |          "fax": "",
+                  |          "website": "https://bluntrochester.house.gov",
+                  |          "webform": "",
+                  |          "congress_office": "1123 Longworth House Office Building",
+                  |          "bioguide_id": "B001303",
+                  |          "votesmart_id": "",
+                  |          "feccandid": "",
+                  |          "twitter_id": "RepBRochester",
+                  |          "youtube_url": "",
+                  |          "facebook_id": "Rep.BluntRochester",
+                  |          "birthdate": "1962-02-10"
+                  |        }
+                  |      },
+                  |      {
+                  |        "@attributes": {
+                  |          "cid": "N00012508",
+                  |          "firstlast": "Tom Carper",
+                  |          "lastname": "Carper",
+                  |          "party": "D",
+                  |          "office": "DES1",
+                  |          "gender": "M",
+                  |          "first_elected": "2000",
+                  |          "exit_code": "0",
+                  |          "comments": "",
+                  |          "phone": "202-224-2441",
+                  |          "fax": "202-228-2190",
+                  |          "website": "https://www.carper.senate.gov/public",
+                  |          "webform": "http://www.carper.senate.gov/public/index.cfm/email-senator-carper",
+                  |          "congress_office": "513 Hart Senate Office Building",
+                  |          "bioguide_id": "C000174",
+                  |          "votesmart_id": "22421",
+                  |          "feccandid": "S8DE00079",
+                  |          "twitter_id": "SenatorCarper",
+                  |          "youtube_url": "https://youtube.com/senatorcarper",
+                  |          "facebook_id": "tomcarper",
+                  |          "birthdate": "1947-01-23"
+                  |        }
+                  |      },
+                  |      {
+                  |        "@attributes": {
+                  |          "cid": "N00031820",
+                  |          "firstlast": "Chris Coons",
+                  |          "lastname": "Coons",
+                  |          "party": "D",
+                  |          "office": "DES2",
+                  |          "gender": "M",
+                  |          "first_elected": "2010",
+                  |          "exit_code": "0",
+                  |          "comments": "",
+                  |          "phone": "202-224-5042",
+                  |          "fax": "202-228-3075",
+                  |          "website": "https://www.coons.senate.gov",
+                  |          "webform": "https://www.coons.senate.gov/contact",
+                  |          "congress_office": "127a Russell Senate Office Building",
+                  |          "bioguide_id": "C001088",
+                  |          "votesmart_id": "122834",
+                  |          "feccandid": "S0DE00092",
+                  |          "twitter_id": "SenCoonsOffice",
+                  |          "youtube_url": "https://youtube.com/senatorchriscoons",
+                  |          "facebook_id": "senatorchriscoons",
+                  |          "birthdate": "1963-09-09"
+                  |        }
+                  |      }
+                  |    ]
+                  |  }
+                  |}""".stripMargin
+  val data = Json.parse(paylaod)
+  val legislators = data \\ "legislator"
+  println(legislators)
 
-    case _ =>
-      println("FAILURE")
-      stop()
-  }
-
-  def stop(): Unit = {
-    context.parent ! Done
-    context.stop(self)
-  }
 
 }
